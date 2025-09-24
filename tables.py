3fd209ce7fd09eb1a0713492a4e5e25b3ed13d24
Javier Sánchez GitKraken <javier.sanchez.castro@bookline.ai>
2025-09-24 21:30:18 +0200

    refactor

--- tables.py
+++ tables.py
@@ -20,6 +20,7 @@ class Question(Base):
     __tablename__ = 'questions'
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     type: Mapped[str] = mapped_column(String)  # 'open', 'exact', 'test', 'true_false', 'matching', 'order'
+    question_text: Mapped[str] = mapped_column(String)
     
     @declared_attr
     def __mapper_args__(cls):
@@ -32,22 +33,18 @@ class OpenQuestion(Question):
     __tablename__ = 'open_questions'
     __mapper_args__ = {"polymorphic_identity": "open"}
     id: Mapped[int] = mapped_column(Integer, ForeignKey('questions.id'), primary_key=True)
-    question_text: Mapped[str] = mapped_column(String)  # The open-ended question text
-
 
 # Exact answer question type
 class ExactQuestion(Question):
     __tablename__ = 'exact_questions'
     __mapper_args__ = {"polymorphic_identity": "exact"}
     id: Mapped[int] = mapped_column(Integer, ForeignKey('questions.id'), primary_key=True)
-    question_text: Mapped[str] = mapped_column(String)
     exact_answer: Mapped[str] = mapped_column(String)  # Exact answer, could be a word or number
 
 class SingleChoiceQuestion(Question):
     __tablename__ = 'single_choice_questions'
     __mapper_args__ = {"polymorphic_identity": "single_choice"}
     id: Mapped[int] = mapped_column(Integer, ForeignKey('questions.id'), primary_key=True)
-    question_text: Mapped[str] = mapped_column(String)
     # Only choice-based questions expose options
     options: Mapped[list["ChoiceOption"]] = relationship(
         "ChoiceOption",
@@ -62,7 +59,6 @@ class MultipleChoiceQuestion(Question):
     __tablename__ = 'multiple_choice_questions'
     __mapper_args__ = {"polymorphic_identity": "multiple_choice"}
     id: Mapped[int] = mapped_column(Integer, ForeignKey('questions.id'), primary_key=True)
-    question_text: Mapped[str] = mapped_column(String)
     
     # Options for the multiple-choice question
     options: Mapped[list["ChoiceOption"]] = relationship(
@@ -90,7 +86,6 @@ class TrueFalseQuestion(Question):
     __tablename__ = 'true_false_questions'
     __mapper_args__ = {"polymorphic_identity": "true_false"}
     id: Mapped[int] = mapped_column(Integer, ForeignKey('questions.id'), primary_key=True)
-    question_text: Mapped[str] = mapped_column(String)
     answer: Mapped[bool] = mapped_column(Boolean)  # True or False answer
 
 
